2e10a3a26182fce60f3a9815859fbb1e295a4220
Davenge Coders <korisd@gmail.com>
2013-03-05 13:33:33 -0700

    Fixed it so that if your target disappears, so does the TARGET_DATA

--- comm.c
+++ comm.c
@@ -3686,8 +3686,13 @@ void display_prompt( DESCRIPTOR_DATA * d )
    }
    *pbuf = '\0';
    if( ch->target )
-      ch_printf( ch, "&cTarget&C:&w %s &cRange&C:&w %d &cDir&C:&w %s&w\r\n", ch->target->victim->name, ch->target->range,
-      ch->target->dir == -1 ? "(null)" : dir_name[ch->target->dir] );
+   {
+      if( ch->target->victim || ch->target->victim->name )
+         ch_printf( ch, "&cTarget&C:&w %s &cRange&C:&w %d &cDir&C:&w %s&w\r\n", ch->target->victim->name, ch->target->range,
+                    ch->target->dir == -1 ? "(null)" : dir_name[ch->target->dir] );
+      else
+         clear_target( ch );
+   }
    send_to_char( buf, ch );
    return;
 }
