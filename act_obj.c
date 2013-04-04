8230891c0aef174337f4f90288ace583a1996f8a
Davenge <korisd@gmail.com>
2013-04-04 02:27:47 -0400

    quick bugfix to do_give to make obj_give for quests work

--- act_obj.c
+++ act_obj.c
@@ -1208,7 +1208,7 @@ void do_give( CHAR_DATA* ch, const char* argument)
    act( AT_ACTION, "$n gives you $p.", ch, obj, victim, TO_VICT );
    act( AT_ACTION, "You give $p to $N.", ch, obj, victim, TO_CHAR );
    obj = obj_to_char( obj, victim );
-   update_quests( victim, NULL, obj, TYPE_OBJ_RECEIVE, 0 );
+   update_quests( ch, victim, obj, TYPE_OBJ_RECEIVE, 0 );
    mprog_give_trigger( victim, ch, obj );
    if( IS_SET( sysdata.save_flags, SV_GIVE ) && !char_died( ch ) )
       save_char_obj( ch );
